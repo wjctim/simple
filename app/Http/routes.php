273b2dafa210ed896cd89f5ea0bd78338e0b0a91
vagrant <vagrant@homestead>
2017-06-17 10:10:28 +0000

    Finish static-pages

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', 'StaticPagesController@home');
+Route::get('/help', 'StaticPagesController@help');
+Route::get('/about', 'StaticPagesController@about');
---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>@yield('title', 'Simple App') -- Laravel test</title>
+</head>
+<body>
+@yield('content')
+</body>
+</html>
