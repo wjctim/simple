e8508a7c208c2c6623fe4ad90def2292ad39afc8
vagrant <wjctim520rong@gmail.com>
2017-07-16 07:52:43 +0000

    Finish sign-up

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use App\Models\User;
 
 class UsersController extends Controller
 {
@@ -14,6 +15,30 @@ class UsersController extends Controller
     public function create()
     {
         return view('users.create');
+    }
+
+    public function show($userId)
+    {
+        $user = User::findOrFail($userId);
+        return view('users.show', compact('user'));
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required'
+        ]);
+
+        $user = User::create([
+            'name' => $request['name'],
+            'email' => $request['email'],
+            'password' => bcrypt($request['password'])
+        ]);
+        session()->flush('success', '欢迎，您将在这里开启一段新的旅程~');
+
+        return redirect()->route('users.show', [$user->id]);
     }
     
 }
--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -1,7 +1,7 @@
 <header class="navbar navbar-fixed-top navbar-inverse">
     <div class="container">
         <div class="col-md-offset-1 col-md-10">
-            <a href="{{ route('home') }}" id="logo">Sample App</a>
+            <a href="{{ route('home') }}" id="logo">Simple App</a>
             <nav>
                 <ul class="nav navbar-nav navbar-right">
                     <li><a href="{{ route('help') }}">帮助</a></li>
